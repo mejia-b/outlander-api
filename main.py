beb351f8eed4086c6c1935f39d7cc10bee2661c0
Brenda <mejiab19@gmail.com>
2023-10-13 11:19:08 -0500

    Add endpoints for GET all seasons and GET single season

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException, status
+from typing import List
+from models import Episode, Season
 import json
 # instatiate app
 app = FastAPI()
 
 # read in json data
 with open('data/outlander.json') as f:
-    show_data = json.load(f)
+    show_data: dict = json.load(f)
 
 # homepage endpoint
 @app.get("/",include_in_schema=False)
 async def get_homepage():
     return {"message": "Hello World"}
 
-# endpoint for all shows
-@app.get("/seasons")
+# endpoint for all seasons
+@app.get("/seasons", response_model=List[Season])
 async def get_seasons():
-    return show_data
+    return show_data["seasons"]
+
+# endpoint for a single season
+@app.get("/seasons/{season_number}", response_model=Season)
+async def get_season(season_number: int):
+    for season in show_data["seasons"]:
+        if season["season_number"] == season_number:
+            return season
+    raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= "Season not found.")
