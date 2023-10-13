c04f744e39d7c9ea06b873d010361be3ed0d6a90
Brenda <mejiab19@gmail.com>
2023-10-13 12:00:30 -0500

    Add endpoint for GET single episode from a specific season

--- main.py
+++ main.py
@@ -29,10 +29,19 @@ async def get_season(season_number: int):
 
 # endpoint for all episodes from a specific season
 @app.get("/seasons/{season_number}/all_episodes", response_model= List[Episode])
-async def get_season_episodes(season_number: int):
+async def get_episodes(season_number: int):
     for season in show_data["seasons"]:
         if season["season_number"] == season_number:
             return season["episodes"]
     raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= "Season not found.")
 
-# endpoint for sinle episode form a specific season
+# endpoint for single episode form a specific season
+@app.get("/seasons/{season_number}/{episode_number}", response_model=Episode)
+async def get_episode(season_number: int, episode_number: int):
+    for season in show_data["seasons"]:
+        if season["season_number"] == season_number:
+            for episode in season["episodes"]:
+                if episode["episode_number"] == episode_number:
+                    return episode
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Episode not found.")
+    raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= "Season not found.")
